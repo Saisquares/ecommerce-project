a117b1226aa04f6535dd4fca94ae33af6daab0bb
Saisquares <darisettisaikumar@gmail.com>
2024-03-02 01:25:14 +0530

    gh pages

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,18 +6,16 @@ import { removeProductInCart } from "../utils/cartProdutsSlice";
 
 const Cart = () => {
   const cartProducts = useSelector((store) => store.cart.cartItems);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const totalAmount = cartProducts.reduce((acc,value) => acc + value.price,0)
+  
+  const handleRemoveCartProduct = (id) => {
+    dispatch(removeProductInCart(id));
+  };
+  if (cartProducts.length === 0) return <CartIsEmpty />;
 
-  const handleRemoveCartProduct  = (id) => {
-    dispatch(removeProductInCart(id))
-  }
-  if (cartProducts.length === 0)
-    return (
-      <CartIsEmpty/>
-    );
-   
   return (
-    <div className="pt-20 w-6/12 md:w-8/12 mx-auto">
+    <div className="pt-20 w-6/12 md:w-6/12 mx-auto">
       <div>
         <h1 className="text-center font-bold text-4xl py-8 border-b-2">
           Shopping Cart
@@ -46,6 +44,18 @@ const Cart = () => {
               />
             )
           )}
+      </div>
+      <div>
+        <div className="flex justify-between my-8">
+          <div>
+            <p className="text-2xl">Subtotal</p>
+            <p className="text-md text-gray-500">Shipping and taxes will be calculated at checkout</p>
+          </div>
+          <div>
+            <p className="text-2xl font-bold">${totalAmount.toFixed(2)}</p>
+          </div>
+        </div>
+        <div className="text-center my-4"> <button className="py-2 w-full bg-blue-700 text-white rounded-sm text-lg">Checkout</button></div>
       </div>
     </div>
   );
