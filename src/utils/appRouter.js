0b74f9ef7e24d019b7c0b4988f3ef9f51d68cfbc
Saisquares <darisettisaikumar@gmail.com>
2024-03-02 14:37:08 +0530

    basename added in appRouter

--- src/utils/appRouter.js
+++ src/utils/appRouter.js
@@ -1,41 +1,46 @@
 import React from "react";
-import { createBrowserRouter } from "react-router-dom"; 
+import { createBrowserRouter } from "react-router-dom";
 import Home from "../components/Home";
 import SignIn from "../components/SignIn";
 import Cart from "../components/Cart";
 import About from "../components/About";
 import Wishlist from "../components/Wishlist";
 import ErrorPage from "../components/ErrorPage";
-import Body from "../components/Body"
+import Body from "../components/Body";
 
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <SignIn />,
-    errorElement: <ErrorPage/>
-  },
-  {
-    path: '/',
-    element: <Body/>,
-    children:[
+const appRouter = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <SignIn />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/",
+      element: <Body />,
+      children: [
         {
-            path: "/home",
-            element: <Home />,
-          },
-          {
-            path: "/cart",
-            element: <Cart />,
-          },
-          {
-            path: "/about",
-            element: <About />,
-          },
-          {
-            path: "/wishlist",
-            element: <Wishlist />,
-          }
-    ]
+          path: "/home",
+          element: <Home />,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/wishlist",
+          element: <Wishlist />,
+        },
+      ],
+    },
+  ],
+  {
+    basename: "/ecommerce-project",
   }
-]);
+);
 
 export default appRouter;
