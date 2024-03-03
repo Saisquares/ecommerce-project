fe13c657a6847c114b18836d20d81d79a4b8a30b
Saisquares <darisettisaikumar@gmail.com>
2024-03-03 13:46:13 +0530

    errorPage updated

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { useRouteError } from 'react-router-dom'
+import { TbError404 } from "react-icons/tb";
 
 const ErrorPage = () => {
   const errorMessage = useRouteError()
 
   return (
-    <div>
-      <h1>{errorMessage.error.message}</h1>
-      <p>status {errorMessage.status} - {errorMessage.statusText}</p>
+    <div className='  sm:8/12 lg:w-6/12 flex flex-col justify-center mx-auto items-center h-screen'>
+      <TbError404  className='text-8xl'/>
+      <h1 className='font-bold text-lg'>{errorMessage.error.message}</h1>
+      <p className='text-gray-600'>status {errorMessage.status} - {errorMessage.statusText}</p>
     </div>
   )
 }
