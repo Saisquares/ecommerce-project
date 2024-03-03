f87582a09f2dd2a276bf2062e84e7cf88118a1ee
Saisquares <darisettisaikumar@gmail.com>
2024-03-04 01:32:34 +0530

    search functionality , search error and loading state added

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,32 @@
 import React from "react";
+import { FaHtml5 } from "react-icons/fa";
+import { MdHtml } from "react-icons/md";
+import { FaCss3Alt } from "react-icons/fa6";
+import { MdCss } from "react-icons/md";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaReact } from "react-icons/fa";
+import { TbBrandRedux } from "react-icons/tb";
+import { SiTailwindcss } from "react-icons/si";
+import { IoLogoFirebase } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa";
 
 const About = () => {
   return (
-    <div className="pt-24 text-center">
-      <h1 className="text-2xl font-bold text-blue-700 ">Shop Hub</h1>
-      <p className=" font-semibold">E Commerce Web Application</p>
-      <p className="text-sm text-gray-500">Created by Saikumar Darisetti</p>
+    <div className="h-screen grid place-content-center  text-center">
+      <h1 className="py-2 text-3xl lg:text-5xl font-bold text-blue-700 ">Shop Hub</h1>
+      <p className="sm:text-xl lg:text-3xl font-semibold">E Commerce Web Application</p>
+      <p className="text-sm lg:py-2 text-gray-500">Created by Saikumar Darisetti</p>
+      <div className="flex justify-center my-2 text-gray-600 text-lg cursor-pointer">
+        <div className="px-1" ><FaHtml5 /><MdHtml /></div>
+        <div className="px-1" ><FaCss3Alt /><MdCss /></div>
+        <div className="px-1" ><IoLogoJavascript /></div>
+        <div className="px-1" ><FaReact /></div>
+        <div className="px-1" ><TbBrandRedux /></div>
+        <div className="px-1" ><SiTailwindcss /></div>
+        <div className="px-1" ><IoLogoFirebase /></div>
+        <div className="px-1" ><FaGithub /></div>
+      
+      </div>
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,65 +1,118 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addProducts } from "../utils/productsSlice";
 import ProductCard from "./ProductCard";
 import { MdFilterAlt } from "react-icons/md";
 import { FaSearch } from "react-icons/fa";
+import { TbMoodSad } from "react-icons/tb";
 
 const Home = () => {
   const products = useSelector((store) => store.products);
   const dispatch = useDispatch();
+  const [searchText, setSearchText] = useState("");
+  const [filterProducts, setFilterProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         const response = await axios.get("https://fakestoreapi.com/products");
         dispatch(addProducts(response.data));
+        setFilterProducts(response.data);
+        setIsLoading(false)
       } catch (error) {
         console.log(error.message);
+        setIsLoading(false)
       }
     };
     fetchProducts();
-  }, []);
+  }, [dispatch]);
 
   return (
-   <div >
-    <div className="w-11/12 md:w-9/12 lg:w-9/12 mx-auto  grid  md:grid-cols-2 lg:grid-cols-2 justify-center place-items-center lg:pb-4 pt-20 md:pt-24 lg:pt-24 gap-2 md:gap-5 lg:gap-5">
-    <div className="place-self-end relative w-full">
-      <input type="text" className="w-96 lg:w-full outline-none px-2 h-8 shadow-md bg-blue-700 text-white placeholder-white font-semibold relative" placeholder="Search Products" />
-      <button className="absolute right-3 py-2 "><FaSearch className="text-white " /></button>
-    </div>
-    <div className=" w-96 md:w-full lg:w-full place-self-start  px-2 pt-1 h-8 bg-blue-700 text-white shadow-md">
-      <div className="flex items-center justify-center cursor-pointer">
-        <div className="font-semibold flex  items-center"><span><MdFilterAlt  className="size-5 font-black" /></span><span>Filters</span></div>
-        <p></p>
+    <div>
+      <div className="w-11/12 md:w-9/12 lg:w-9/12 mx-auto  grid  md:grid-cols-2 lg:grid-cols-2 justify-center place-items-center lg:pb-4 pt-20 md:pt-24 lg:pt-24 gap-2 md:gap-5 lg:gap-5">
+        <div className="place-self-end relative w-full">
+          <input
+            type="text"
+            className="w-96 lg:w-full outline-none px-2 h-8 shadow-md bg-blue-700 text-white placeholder-white font-semibold relative"
+            placeholder="Search Products"
+            name="searchText"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+              const FilterProducts = products.filter((product) =>
+                product.title
+                  .toLowerCase()
+                  .includes(searchText.toLocaleLowerCase())
+              );
+              setFilterProducts(FilterProducts);
+            }}
+            id="searchText"
+          />
+          <button
+            onClick={() => {
+              const FilterProducts = products.filter((product) =>
+                product.title
+                  .toLowerCase()
+                  .includes(searchText.toLocaleLowerCase())
+              );
+              setFilterProducts(FilterProducts);
+            }}
+            className="absolute right-3 py-2 "
+          >
+            <FaSearch className="text-white " />
+          </button>
+        </div>
+        <div className=" w-96 md:w-full lg:w-full place-self-start  px-2 pt-1 h-8 bg-blue-700 text-white shadow-md">
+          <div className="flex items-center justify-center cursor-pointer">
+            <div className="font-semibold flex  items-center">
+              <span>
+                <MdFilterAlt className="size-5 font-black" />
+              </span>
+              <span>Filters</span>
+            </div>
+            <p></p>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-     <div className="pt-5 flex flex-wrap mx-auto justify-center ">
-      {products &&
-        products.map(
-          (
-            { id, title, description, price, image, rating, category },
-            index
-          ) => (
-            <ProductCard
-              key={id}
-              data={{
-                id,
-                title,
-                description,
-                price,
-                image,
-                rating,
-                category,
-                index,
-              }}
-            />
+      
+      <div className="pt-5 flex flex-wrap mx-auto justify-center ">
+      {isLoading ? (
+          <div className="m-10 font-bold text-xl">Loading...</div>
+        ) : filterProducts.length === 0 ? (
+          <div className="flex flex-col justify-center items-center my-36">
+            <TbMoodSad className="text-5xl" />
+            <h1 className="font-bold text-xl">Whoops!</h1>
+            <p className="text-gray-500">
+              Sorry, but nothing matched your search.
+            </p>
+            <p className="text-gray-500">Please try some different keywords</p>
+          </div>
+        ) : (
+          filterProducts.map(
+            (
+              { id, title, description, price, image, rating, category },
+              index
+            ) => (
+              <ProductCard
+                key={id}
+                data={{
+                  id,
+                  title,
+                  description,
+                  price,
+                  image,
+                  rating,
+                  category,
+                  index,
+                }}
+              />
+            )
           )
         )}
+      </div>
     </div>
-   </div>
   );
 };
 
