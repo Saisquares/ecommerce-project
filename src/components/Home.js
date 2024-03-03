a6c1b6af31710b2d4a5f6d7f0ff6b177fe2cf29d
Saisquares <darisettisaikumar@gmail.com>
2024-03-03 23:19:17 +0530

    search & filter width updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,12 +24,12 @@ const Home = () => {
 
   return (
    <div >
-    <div className="w-full grid md:grid-cols-2 lg:grid-cols-2 justify-center place-items-center lg:pb-4 pt-20 md:pt-24 lg:pt-24 gap-2 md:gap-5 lg:gap-5">
-    <div className="place-self-end relative">
-      <input type="text" className="w-96 outline-none px-2 h-8 shadow-md bg-blue-700 text-white placeholder-white font-semibold relative" placeholder="Search Products" />
+    <div className="w-11/12 md:w-9/12 lg:w-9/12 mx-auto  grid  md:grid-cols-2 lg:grid-cols-2 justify-center place-items-center lg:pb-4 pt-20 md:pt-24 lg:pt-24 gap-2 md:gap-5 lg:gap-5">
+    <div className="place-self-end relative w-full">
+      <input type="text" className="w-96 lg:w-full outline-none px-2 h-8 shadow-md bg-blue-700 text-white placeholder-white font-semibold relative" placeholder="Search Products" />
       <button className="absolute right-3 py-2 "><FaSearch className="text-white " /></button>
     </div>
-    <div className="place-self-start w-96 px-2 pt-1 h-8 bg-blue-700 text-white shadow-md">
+    <div className=" w-96 md:w-full lg:w-full place-self-start  px-2 pt-1 h-8 bg-blue-700 text-white shadow-md">
       <div className="flex items-center justify-center cursor-pointer">
         <div className="font-semibold flex  items-center"><span><MdFilterAlt  className="size-5 font-black" /></span><span>Filters</span></div>
         <p></p>
